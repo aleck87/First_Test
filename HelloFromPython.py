fc92e242230eb3fb98b4879fe266cb9546f1cb83
kdm1kor <deepak.kumar2@de.bosch.com>
2023-06-29 13:26:02 +0200

    some update

--- HelloFromPython.py
+++ HelloFromPython.py
@@ -2,10 +2,10 @@ print('Hello there')
 
 nums = [1,234,35,46,7,578,8768,6]
 
-for num in range(len(nums)):
-    if num(i) > 10:
-        print(num(i))
-        
-Print('This is for the new branch Experiment')
+for num in nums:
+    if num> 10:
+        print(num)
+
+print('This is for the new branch Experiment')
 
 # This is the push from local repo
